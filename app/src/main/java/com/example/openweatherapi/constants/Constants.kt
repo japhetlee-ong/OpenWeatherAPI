8f6c3a6d6a1b76236603c722372b1307a13c00e4
japhetlee-ong <japhetlee.dev@gmail.com>
2022-11-14 12:57:42 +0800

    Update Constants.kt

--- app/src/main/java/com/example/openweatherapi/constants/Constants.kt
+++ app/src/main/java/com/example/openweatherapi/constants/Constants.kt
@@ -1,4 +1,4 @@
 package com.example.openweatherapi.constants
 
 const val BASE_URL = "http://api.openweathermap.org/"
-const val API_KEY = "dbdedef2bdbd033f844341f09f432907"
+const val API_KEY = "API_KEY_HERE"
